ad74c7cf5f5ef29087884934ccd5d8859c6cee7a
eezTool <theashes@qq.com>
2023-07-12 04:55:21 +0800

    加了json字符串化的函数

--- index.js
+++ index.js
@@ -1,21 +1,80 @@
 // 广告：陈一发儿 2023 直播导航：67373.net
 let exp = module.exports;
 
-/*✳️ promise 形式简单的终端输入（官方已经有了，但是这个是为了向低版本node兼容） */
-exp.clAsk = async function (questionStr) {
+/*✳️✳️✳️✳️✳️✳️✳️✳️
+json 转为字符串，绕开了 JSON.stringify 循环报错 */
+exp.jsonToStr = function (obj, params = {}) {
+  let {
+    indentation = 2,
+    ignoreType = ['string']
+  } = params;
+  const seen = new WeakSet();
+  for (let item of ignoreType) {
+    if (typeof (obj) == item) return obj;
+  }
+  return JSON.stringify(obj, (key, value) => {
+    if (typeof value === 'object' && value !== null) {
+      if (seen.has(value)) return '[Circular]';
+      seen.add(value);
+    };
+    if (value === false) {
+      return 'false';
+    } else if (value === null) {
+      return 'null';
+    } else if (value === 0) {
+      return 0;
+    } else if (value === '') {
+      return '';
+    } else if (value === undefined) {
+      return 'undefined';
+    } else if (Number.isNaN(value)) {
+      return 'NaN';
+    } else return value;
+  }, indentation);
+};
+
+/*✳️✳️✳️✳️✳️✳️✳️✳️
+promise 形式简单的终端输入，可设置倒计时默认答案*/
+exp.clAsk = async function (q, a, t) {
+  t = Number(t);
   return new Promise((resolve, reject) => {
     const readline = require('readline').createInterface({
       input: process.stdin,
       output: process.stdout,
     });
-    readline.question(questionStr, answer => {
+    readline.question(q, (answer) => {
       resolve(answer);
       readline.close();
     });
+    if (t != 0) {
+      setTimeout(() => {
+        // console.log(a);
+        resolve(a);
+        readline.close();
+      }, t);
+    };
   })
 };
 
-/*✳️ 比较适合中文的文件名合法化：
+/*✳️✳️✳️✳️✳️✳️✳️✳️
+控制台倒计时*/
+exp.countdown = function (seconds) {
+  let remainingSeconds = Number(seconds);
+  const intervalId = setInterval(() => {
+    process.stdout.clearLine(0); // 清除当前行
+    process.stdout.cursorTo(0); // 将光标移动到行的开始位置
+    process.stdout.write(String(remainingSeconds)); // 在同一行输出倒计时数字
+    remainingSeconds--;
+    if (remainingSeconds < 0) {
+      clearInterval(intervalId);
+      process.stdout.write('\n'); // 输出换行符，完成倒计时
+    }
+  }, 1000);
+  return (intervalId);
+};
+
+/*✳️✳️✳️✳️✳️✳️✳️✳️
+比较适合中文的文件名合法化：
   1、将 Reserved characters (/, ?, <, >, \, :, *, |, and ")换成全角；
   2、将 \n \r 换成全角＼；
   3、最后交给第三方的 sanitize-filename 处理：https://www.npmjs.com/package/sanitize-filename */
@@ -31,7 +90,8 @@ exp.goodFilename = function (str) {
   return sanitizeFilename(str);
 };
 
-/* ✳️ formatTime 返回特定时区的格式时间。*/
+/*✳️✳️✳️✳️✳️✳️✳️✳️
+formatTime 返回特定时区的格式时间。*/
 /*  ← 默认：2023年02月06日[周一]16:26:56.918 */
 /*  ← 230206一 formatTime({style: '[YY][MM][DD][xq]'}); */
 /*  ← 230206_163021 formatTime({style: '[YY][MM][DD]_[hh][mm][ss]'}); */
@@ -59,7 +119,8 @@ exp.formatTime = function (params = {}) {
   return style;
 };
 
-/* ✳️ 等待毫秒 */
+/*✳️✳️✳️✳️✳️✳️✳️✳️
+等待毫秒 */
 exp.wait = async function (ms = 1000) {
   return new Promise((resolve, reject) => {
     setTimeout(() => { resolve(ms) }, ms)
